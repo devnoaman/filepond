1e5e5dfa2311cfe7325ddcdccc15e6d8969f51bf
Noaman Monther <noman.monther@transforat.com>
2026-07-07 10:07:28 +0300

    refactor: remove FilepondController and clean up example project configuration

--- apps/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ apps/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,7 +9,6 @@ import file_picker
 import file_selector_macos
 import filepond
 import flutter_image_compress_macos
-import path_provider_foundation
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -17,6 +16,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FilepondPlugin.register(with: registry.registrar(forPlugin: "FilepondPlugin"))
   FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
